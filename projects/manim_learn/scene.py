0ad4520badacb4c6b682a934e524a83037884dd4
Martin Michalski <martmichals@gmail.com>
2022-10-27 16:40:11 -0500

    Created scene with tokenization

--- projects/manim_learn/scene.py
+++ projects/manim_learn/scene.py
@@ -1,4 +1,7 @@
+import re
+import math
 from manim import *
+import numpy as np
 
 class Logo(Scene):
     def construct(self):
@@ -13,6 +16,120 @@ class Logo(Scene):
         self.wait()
         
         # Wipe the screen
-        self.play(Unwrite(subtitle, run_time=0.75))
-        self.play(Unwrite(title, run_time=0.75))
+        self.play(
+            Unwrite(subtitle, run_time=0.75), 
+            Unwrite(title, run_time=0.75)
+        )
         self.wait(0.10)
+
+SPACES_TO_INSERT = 10
+class SimpleTokenizationExample(Scene):
+    def construct(self):
+        # Sample text
+        sent_text = 'I got groceries from the store.'
+        sent = Text(sent_text, font_size=30)
+
+        # Space out the tokens
+        sent_split_text = sent_text.replace(' ', ' '*SPACES_TO_INSERT)
+        sent_split = Text(sent_split_text, font_size=30)
+
+        # Write the sample to the screen
+        self.play(Write(sent))
+        # self.wait(0.5)
+
+        # Send to the top of the screen
+        self.play(sent.animate.to_edge(UP))
+        # self.wait()
+
+        # Indices of spaces in the original sentence
+        space_indices = [(m.end()-m.start()-1)/2 + m.start() for m in re.finditer(r'\w+', sent_text)]
+        space_indices = [(math.floor(idx), math.ceil(idx)) for idx in space_indices]
+
+        # Compute start coordinates for arrows
+        arrow_starts = [
+            np.array([
+                sent[idx[0]-idxnum].get_bottom(), 
+                sent[idx[1]-idxnum].get_bottom()
+            ]).mean(0)
+        for idxnum, idx in enumerate(space_indices)]
+        arrow_ends = [
+            np.array([
+                sent_split[idx[0]-idxnum].get_top(), 
+                sent_split[idx[1]-idxnum].get_top()
+            ]).mean(0)
+        for idxnum, idx in enumerate(space_indices)]
+
+        # Plot the tokenized sentence
+        arrows = [
+            Arrow(
+                start=s, 
+                end=e, 
+                buff=0.2, 
+                max_tip_length_to_length_ratio=0.06,
+                max_stroke_width_to_length_ratio=1
+            ) 
+        for s, e in zip(arrow_starts, arrow_ends)]
+        self.play(Create(VGroup(*arrows)), Write(sent_split))
+        self.play(Uncreate(VGroup(*arrows)))
+        # self.wait()
+
+        # Highlight the good portion of the sentence
+        self.play(
+            sent_split[:20].animate.set_color(GREEN),
+            sent_split[20:].animate.set_color(RED)
+        )
+
+class TokenizationExampleWithPunc(Scene):
+    def construct(self):
+        # Sample text
+        sent_text = 'I got groceries from the store.'
+        sent = Text(sent_text, font_size=30)
+
+        # Space out the tokens
+        sent_text = 'I got groceries from the store .'
+        sent_split_text = sent_text.replace(' ', ' '*SPACES_TO_INSERT)
+        sent_split = Text(sent_split_text, font_size=30)
+
+        # Write the sample to the screen
+        self.play(Write(sent))
+        # self.wait(0.5)
+
+        # Send to the top of the screen
+        self.play(sent.animate.to_edge(UP))
+        # self.wait()
+
+        # Indices of spaces in the original sentence
+        space_indices = [(m.end()-m.start()-1)/2 + m.start() for m in re.finditer(r'\w+', sent_text)]
+        space_indices = [(math.floor(idx), math.ceil(idx)) for idx in space_indices]
+
+        # Compute start coordinates for arrows
+        arrow_starts = [
+            np.array([
+                sent[idx[0]-idxnum].get_bottom(), 
+                sent[idx[1]-idxnum].get_bottom()
+            ]).mean(0)
+        for idxnum, idx in enumerate(space_indices)]
+        arrow_ends = [
+            np.array([
+                sent_split[idx[0]-idxnum].get_top(), 
+                sent_split[idx[1]-idxnum].get_top()
+            ]).mean(0)
+        for idxnum, idx in enumerate(space_indices)]
+
+        # Plot the tokenized sentence
+        arrows = [
+            Arrow(
+                start=s, 
+                end=e, 
+                buff=0.2, 
+                max_tip_length_to_length_ratio=0.06,
+                max_stroke_width_to_length_ratio=1
+            ) 
+        for s, e in zip(arrow_starts, arrow_ends)]
+        self.play(Create(VGroup(*arrows)), Write(sent_split))
+        self.play(Uncreate(VGroup(*arrows)))
+        # self.wait()
+
+        # Highlight the good portion of the sentence
+        self.play(sent_split.animate.set_color(GREEN))
+
